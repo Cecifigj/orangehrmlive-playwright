fc3ce9adaac91558a9d3e50d482c8e205738a867
CeFig <cfigueroa@360insights.com>
2025-05-25 16:36:30 -0300

    adding changes

--- tests/sanityTest.spec.ts
+++ tests/sanityTest.spec.ts
@@ -6,13 +6,14 @@ let login : LoginPage;
 let homepage : Homepage;
 
 test.beforeEach("Admin Login", async({ page })=> {
-    await page.goto("https://opensource-demo.orangehrmlive.com/");
+    await page.goto("https://opensource-demo.orangehrmlive.com/"); 
     login = new LoginPage(page);
     homepage = new Homepage(page);
     await login.Login("Admin","admin123");
 
 });
-
+test.describe("Tests created for challenge", () => {
 test("Verify Job Details @sanity", async({ page })=> {
     await homepage.Job_Details();
+})
 })
